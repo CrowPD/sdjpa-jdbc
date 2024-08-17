60df0117997655da382f4cb2ce322202a3e24f22
CrowPD <anubis.dead.god@gmail.com>
2024-08-17 11:00:22 +0300

    Added sql script tracing and debug logging. Implemented search functions using Hibernate API

--- src/main/java/guru/springframework/sdjpa_jdbc/dao/hibernate/HiberAuthorDaoImpl.java
+++ src/main/java/guru/springframework/sdjpa_jdbc/dao/hibernate/HiberAuthorDaoImpl.java
@@ -2,18 +2,33 @@ package guru.springframework.sdjpa_jdbc.dao.hibernate;
 
 import guru.springframework.sdjpa_jdbc.dao.AuthorDao;
 import guru.springframework.sdjpa_jdbc.domain.Author;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HiberAuthorDaoImpl implements AuthorDao {
+	private final EntityManagerFactory emf;
+
+	public HiberAuthorDaoImpl(EntityManagerFactory emf) {
+		this.emf = emf;
+	}
+
 	@Override
 	public Author getById(Long id) {
-		return null;
+		return getEntityManager().find(Author.class, id);
 	}
 
 	@Override
 	public Author findByName(String firstName, String lastName) {
-		return null;
+		TypedQuery<Author> q = getEntityManager().createQuery(
+				"SELECT a FROM Author a WHERE a.firstName = :firstName AND a.lastName = :lastName",
+				Author.class
+		);
+		q.setParameter("firstName", firstName);
+		q.setParameter("lastName", lastName);
+		return q.getSingleResult();
 	}
 
 	@Override
@@ -29,5 +44,9 @@ public class HiberAuthorDaoImpl implements AuthorDao {
 	@Override
 	public void deleteById(Long id) {
 
+	}
+
+	private EntityManager getEntityManager() {
+		return emf.createEntityManager();
 	}
 }
--- src/main/java/guru/springframework/sdjpa_jdbc/dao/hibernate/HibernateBookDaoImpl.java
+++ src/main/java/guru/springframework/sdjpa_jdbc/dao/hibernate/HibernateBookDaoImpl.java
@@ -2,18 +2,32 @@ package guru.springframework.sdjpa_jdbc.dao.hibernate;
 
 import guru.springframework.sdjpa_jdbc.dao.BookDao;
 import guru.springframework.sdjpa_jdbc.domain.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HibernateBookDaoImpl implements BookDao {
+	private final EntityManagerFactory emf;
+
+	public HibernateBookDaoImpl(EntityManagerFactory emf) {
+		this.emf = emf;
+	}
+
 	@Override
 	public Book getById(Long id) {
-		return null;
+		return getEntityManager().find(Book.class, id);
 	}
 
 	@Override
 	public Book findByTitle(String title) {
-		return null;
+		TypedQuery<Book> q = getEntityManager().createQuery(
+				"SELECT b FROM Book b WHERE b.title = :title",
+				Book.class
+		);
+		q.setParameter("title", title);
+		return q.getSingleResult();
 	}
 
 	@Override
@@ -29,5 +43,9 @@ public class HibernateBookDaoImpl implements BookDao {
 	@Override
 	public void deleteById(Long id) {
 
+	}
+
+	private EntityManager getEntityManager() {
+		return emf.createEntityManager();
 	}
 }
